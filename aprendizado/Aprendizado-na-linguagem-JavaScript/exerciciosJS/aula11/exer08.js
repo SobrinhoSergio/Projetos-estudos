19a52280dc66b7fa885a92f6516a9b511de7cc43
Sérgio Sobrinho <sobrinhosergio00@gmail.com>
2020-06-16 17:12:10 -0300

    atualizei

--- aprendizado/Aprendizado-na-linguagem-JavaScript/exerciciosJS/aula11/exer08.js
+++
@@ -1,9 +0,0 @@
-var vel = 68.9
-console.log(`A velocidade é ${vel} KM/h`)
-
-if(vel>=60){ //condição simples
-    console.log("Você ultrapassou a velocidade permitida! Está multado.")
-
-}
-
-console.log("Dirija sempre usando o sinto de segurança!")
